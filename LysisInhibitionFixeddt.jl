434e0effd79d1fbc172c4ef37bd1698e400d0642
Namiko Mitarai <mitarai@nbi.ku.dk>
2024-07-29 15:13:43 +0200

    Save the data and make new plots without running new simulation again

--- LysisInhibitionFixeddt.jl
+++ LysisInhibitionFixeddt.jl
@@ -4,6 +4,7 @@
 using Random
 using Plots
 using Distributions
+using JLD2
 
 # Main function
 function simulate_population_agents(Bstate, Pstate, Istate, LORstate, time_step, record_time_step, final_time, bacteria, phage, infected, bacteriaever, carrying_capacity, 
@@ -238,6 +239,9 @@ time2, Btimeseries2, Itimeseries2, Ptimeseries2, irecord2 = simulate_population_
     li_collapse=li_collapse, li_collapse_phage=li_collapse_phage
 )
 
+# Save the data
+@save "population_data.jld2" time2 Btimeseries2 Itimeseries2 Ptimeseries2 irecord2
+
 # Create the plot
 plot(time2[1:irecord2], Btimeseries2[1:irecord2], label="Bacteria", linewidth=2)
 plot!(time2[1:irecord2], Itimeseries2[1:irecord2], label="Infected Bacteria", linewidth=2)
@@ -256,3 +260,17 @@ savefig("population_dynamics_plot.png")
 
 
 
+# Calculate the difference in Phage levels
+P_diff = diff(Ptimeseries2[1:irecord2])
+
+# Create a new figure for the difference plot
+plot(size=(800, 480))
+
+# Plot the difference in Phage levels
+plot!(time2[2:irecord2] .+ 15, P_diff, label="Difference in Phage Level")
+
+# Label the axes
+xlabel!("Time (minutes)")
+ylabel!("Difference in Phage Level")
+
+savefig("phage_difference_plot.png")
